5d3adf43acb74f30b86f3fb0e9dbd2bfb21e82d7
“bradeu” <bradeu01@gmail.com>
2024-12-29 23:44:31 -0800

    update agent

--- app/models/agent.py
+++ app/models/agent.py
@@ -48,7 +48,8 @@ class Agent:
     def call_openai_breakdown_tasks(self, state: AgentState):
         messages = state['messages']
 
-        system_query = """You are a breakdown agent, your task is to break down the query into multiple executable steps based on the available tools\You are allowed to make multiple calls (either together or in sequence). \
+        system_query = """You are a breakdown sub-agent, your task is to break down the query into multiple executable steps based on the available tools. \
+            You are allowed to make multiple calls (either together or in sequence). \
             Only look up information when you are sure of what you want. \
             If you need to look up some information before asking a follow up question, you are allowed to do that!"""
 
@@ -58,10 +59,26 @@ class Agent:
         return new_state
     
     def call_openai_qualification(self, state: AgentState):
-        pass
+        initial_message = state["messages"][0]
+        messages = state['messages']
+
+        system_query = f"You are a qualification sub-agent, your task is to make sure that the informations provided covered fully this query: {initial_message}."
+
+        messages = [SystemMessage(content=system_query)] + messages
+        message = self.model.invoke(messages)
+        new_state = {'messages': [message]}
+        return new_state
 
     def call_openai_decision(self, state: AgentState):
-        pass
+        initial_message = state["messages"][0]
+        messages = state['messages']
+
+        system_query = f"You are a decision sub-agent, your task is to decide which booking is the best based on this query: {initial_message}."
+
+        messages = [SystemMessage(content=system_query)] + messages
+        message = self.model.invoke(messages)
+        new_state = {'messages': [message]}
+        return new_state
 
     def exists_action(self, state: AgentState):
         result = state['messages'][-1]
